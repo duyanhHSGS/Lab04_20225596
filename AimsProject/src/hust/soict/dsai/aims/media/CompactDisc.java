45d3e9363e32c19a9b993b5065ea262a051116e6
Laptop <example@email.com>
2024-11-30 13:59:11 +0700

    add playable interface, implement it in the previous classes

--- AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -2,7 +2,7 @@ package hust.soict.dsai.aims.media;
 
 import java.util.ArrayList;
 
-public class CompactDisc extends Disc {
+public class CompactDisc extends Disc implements Playable {
     private String artist;
     private ArrayList<Track> tracks = new ArrayList<>();
     public CompactDisc() {
