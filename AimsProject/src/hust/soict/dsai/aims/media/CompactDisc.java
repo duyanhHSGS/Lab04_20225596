3ab81a291571c22b6fb92a5d8cf0e3196166a852
Laptop <example@email.com>
2024-11-30 13:36:28 +0700

    added compact disk inherited from abstract class disk

---
+++ AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -0,0 +1,50 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.ArrayList;
+
+public class CompactDisc extends Disc {
+    private String artist;
+    private ArrayList<Track> tracks = new ArrayList<>();
+    public CompactDisc() {
+        super();
+    }
+    // comprehensive Constructor
+    public CompactDisc(String title, String category, String director, int length, float cost, String artist) {
+        super(title, category, director, length, cost);
+        this.artist = artist;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public void addTrack(Track track) {
+        if (!tracks.contains(track)) {
+            tracks.add(track);
+            System.out.println("Track " + track.getTitle() + " has been successfully added.");
+        } else {
+            System.out.println("Track " + track.getTitle() + " is already in the list.");
+        }
+    }
+    public void removeTrack(Track track) {
+        if (tracks.contains(track)) {
+            tracks.remove(track);
+            System.out.println("Track " + track.getTitle() + " has been successfully removed.");
+        } else {
+            System.out.println("Track " + track.getTitle() + " is not in the list.");
+        }
+    }
+    public float getLength() {
+        float totalLength = 0;
+        for (Track track : tracks) {
+            totalLength += track.getLength();
+        }
+        return totalLength;
+    }
+//    public String toString() {
+//        return "CD - " + title + 
+//               " - " + (category != null ? category : "Unknown") +
+//               " - " + (director != null ? director : "Unknown") +
+//               " - " + (length > 0 ? length + " mins" : "Unknown Length") +
+//               " - Artist: " + (artist != null ? artist : "Unknown") +
+//               ": " + cost + " $";
+//    }
+}
