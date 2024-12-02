455af3fe3db0b470d09da3ae7161c65e2600cf39
Laptop <example@email.com>
2024-12-02 13:14:17 +0700

    Fixed Class Diagram

--- AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -1,96 +1,91 @@
 package hust.soict.dsai.aims;
 
-//import java.util.Vector;
-//
-//import hust.soict.dsai.aims.cart.Cart;
-//import hust.soict.dsai.aims.media.DigitalVideoDisc;
-//
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import hust.soict.dsai.aims.media.Book;
+import hust.soict.dsai.aims.media.CompactDisc;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
+import hust.soict.dsai.aims.media.Track;
+import hust.soict.dsai.aims.store.Store;
+
 public class Aims {
     public static void main(String[] args) {
-//    	Vector<DigitalVideoDisc> storeDvds = new Vector<>();
-////		debugging process 	
-////    	File file = new File("data.txt");
-////      System.out.println("Absolute Path: " + file.getAbsolutePath());
-//
-//        Cart anOrder = new Cart();
-//        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Disk 1", "Categ1", 300);
-//        storeDvds.add(dvd1);
-//        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Disk 2", "Categ2", "Director1",180, 300);
-//        storeDvds.add(dvd2);
-//        // adding more disc using this way is not comfortable
-//        // i used reading in a text file should be cleaner & more comfortable
-//        
-//        try {
-//        	BufferedReader br = new BufferedReader(
-//        			new FileReader("data.txt"));
-//            String line;
-//            while ((line = br.readLine()) != null) {
-//                String[] dvdInfo = line.split(",");
-//                DigitalVideoDisc dvd;
-//                if (dvdInfo.length == 1) {
-//                    dvd = new DigitalVideoDisc(dvdInfo[0]);
-//                } else if (dvdInfo.length == 3) {
-//                    dvd = new DigitalVideoDisc(dvdInfo[0], dvdInfo[1], Float.parseFloat(dvdInfo[2]));
-//                } else if (dvdInfo.length == 5) {
-//                    dvd = new DigitalVideoDisc(
-//                        dvdInfo[0],
-//                        dvdInfo[1],
-//                        dvdInfo[2],
-//                        Integer.parseInt(dvdInfo[3]),
-//                        Float.parseFloat(dvdInfo[4]) 
-//                    );
-//                   // add more disc using method implemented in the dvd class
-//                } else {
-//                    continue; //skip unreadable lines
-//                }
-//                storeDvds.add(dvd);
-//            }
-//            br.close();
-//        } catch (IOException e) {
-//            e.printStackTrace(); //in case it failed
-//        }
-//        System.out.println("Listing all disks in the store:");
-//        for ( int i =0 ; i < storeDvds.size() ; i++) {
-//            System.out.println( "storeDvds[" + i + "] : Title: " + storeDvds.get(i).getTitle());
-//        }
-//        System.out.println("Listing completed");
-//        // storeDvds was initiated successfully
-//        
-//        // ordering
-//        anOrder.addDigitalVideoDisc(dvd2);
-//        anOrder.addDigitalVideoDisc(storeDvds.get(0));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(0));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(1));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(6));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(15));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(2));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(3));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(4));
-//        anOrder.addDigitalVideoDisc(dvd1);
-//        anOrder.addDigitalVideoDisc(dvd2);
-//        anOrder.addDigitalVideoDisc(storeDvds.get(0));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(0));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(1));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(6));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(15));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(2));
-//        //the output starts returning (almost full) messages
-//        anOrder.addDigitalVideoDisc(storeDvds.get(3));
-//        anOrder.addDigitalVideoDisc(storeDvds.get(4));
-//        anOrder.addDigitalVideoDisc(dvd1);
-//        //cart full here
-//        anOrder.addDigitalVideoDisc(dvd1);
-//        //returning (cannot add more)
-//        System.out.println("Total cost: "+ anOrder.totalCost() + " dollar(s)");
-//        anOrder.removeDigitalVideoDisc(storeDvds.get(1));
-//        //remove "Disk 2" from the cart
-//        anOrder.removeDigitalVideoDisc(storeDvds.get(18));
-//        //remove "Disk 19" from the cart
-//        //returning (not exits) message
-//        System.out.println("Total cost: "+ anOrder.totalCost() + " dollar(s)");
+        Store store = new Store();
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] mediaInfo = line.split(",");
+                Media media = null;
+                switch (mediaInfo[0]) {
+                    case "DVD":
+                        if (mediaInfo.length == 2) {
+                            media = new DigitalVideoDisc(mediaInfo[1]);
+                        } else if (mediaInfo.length == 4) {
+                            media = new DigitalVideoDisc(mediaInfo[1], mediaInfo[2], Float.parseFloat(mediaInfo[3]));
+                        } else if (mediaInfo.length == 6) {
+                            media = new DigitalVideoDisc(
+                                mediaInfo[1],
+                                mediaInfo[2],
+                                mediaInfo[3],
+                                Integer.parseInt(mediaInfo[4]),
+                                Float.parseFloat(mediaInfo[5])
+                            );
+                        }
+                        break;
+                    case "Book":
+                        if (mediaInfo.length >= 4) {
+                            Book book = new Book();
+                            book.setTitle(mediaInfo[1]);
+                            book.setCategory(mediaInfo[2]);
+                            book.setCost(Float.parseFloat(mediaInfo[3]));
+                            for (int i = 4; i < mediaInfo.length; i++) {
+                                book.getAuthors().add(mediaInfo[i]);
+                            }
+                            media = book;
+                        }
+                        break;
+                    case "CD":
+                        if (mediaInfo.length >= 6) {
+                            CompactDisc cd = new CompactDisc(
+                                mediaInfo[1],
+                                mediaInfo[2],
+                                mediaInfo[3],
+                                Integer.parseInt(mediaInfo[4]),
+                                Float.parseFloat(mediaInfo[5]),
+                                mediaInfo[6]
+                            );
+                            media = cd;
+                            if (media != null) {
+                                for (int i = 7; i < mediaInfo.length; i += 2) {
+                                    if (i + 1 < mediaInfo.length) {
+                                        Track track = new Track(mediaInfo[i], Integer.parseInt(mediaInfo[i + 1]));
+                                        cd.addTrack(track);
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        continue; // skip
+                }
+                if (media != null) {
+                    store.addMedia(media);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        store.displayStore(); // Test the display method
+
+        store.removeMedia(0); // Harry Potter destroyed
+        store.displayStore();
+// The current implementation limits on ascending DVD's ID
+// The book and CD's ID unimplemented, It didn't mentioned in the PDF 
+        store.removeMedia(1); // Lion King
+        store.displayStore();
     }
 }
